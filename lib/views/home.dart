a69f36a40e9596f1f30271b2862fff487832d3d7
Eryk Lorenc <reus2000@wp.pl>
2023-02-09 00:13:50 +0100

    Add songs and categories

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_ui_clone/widgets/album_card.dart';
+import 'package:spotify_ui_clone/widgets/song_card.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -32,158 +33,273 @@ class _HomeView extends State<HomeView> {
           ),
           SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
-            child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.white.withOpacity(0),
+                    Colors.black.withOpacity(1)
+                  ],
+                ),
+              ),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Recently Played',
+                              style: Theme.of(context).textTheme.headlineSmall),
+                          Row(
+                            children: const [
+                              Icon(Icons.history),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Icon(Icons.settings),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      physics: const BouncingScrollPhysics(),
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        children: const [
+                          AlbumCard(
+                            label: 'Best Mode',
+                            image: AssetImage('assets/album3.jpg'),
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          AlbumCard(
+                            label: 'Relax Mode',
+                            image: AssetImage('assets/album1.jpg'),
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          AlbumCard(
+                            label: 'Best Songs - Global',
+                            image: AssetImage('assets/album2.jpg'),
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          AlbumCard(
+                            label: 'Rap Mode',
+                            image: AssetImage('assets/album4.jpg'),
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          AlbumCard(
+                            label: 'Gaming Mode',
+                            image: AssetImage('assets/album5.jpg'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Good evening',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          Row(
+                            children: const [
+                              RowAlbumCard(
+                                label: 'TOP 50 - Global',
+                                image: AssetImage('assets/top50.jpg'),
+                              ),
+                              SizedBox(
+                                width: 14,
+                              ),
+                              RowAlbumCard(
+                                label: 'Pop Remix',
+                                image: AssetImage('assets/album6.jpg'),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          Row(
+                            children: const [
+                              RowAlbumCard(
+                                label: 'TOP 50 - USA',
+                                image: AssetImage('assets/album13.jpg'),
+                              ),
+                              SizedBox(
+                                width: 14,
+                              ),
+                              RowAlbumCard(
+                                label: 'Workout',
+                                image: AssetImage('assets/album12.jpg'),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          Row(
+                            children: const [
+                              RowAlbumCard(
+                                label: 'Best of NF',
+                                image: AssetImage('assets/album9.jpg'),
+                              ),
+                              SizedBox(
+                                width: 14,
+                              ),
+                              RowAlbumCard(
+                                label: 'Beats to Study',
+                                image: AssetImage('assets/album10.jpg'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Recently Played',
-                            style: Theme.of(context).textTheme.headlineSmall),
-                        Row(
-                          children: const [
-                            Icon(Icons.history),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Icon(Icons.settings),
-                          ],
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            'Based on your recent listening',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          physics: const BouncingScrollPhysics(),
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Row(
+                            children: const [
+                              SongCard(
+                                image: AssetImage('assets/album12.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album10.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album2.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album4.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album1.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album6.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                            ],
+                          ),
                         )
                       ],
                     ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(),
-                    padding: const EdgeInsets.all(20),
-                    child: Row(
-                      children: const [
-                        AlbumCard(
-                          label: 'Best Mode',
-                          image: AssetImage('assets/album3.jpg'),
-                        ),
-                        SizedBox(
-                          width: 14,
-                        ),
-                        AlbumCard(
-                          label: 'Relax Mode',
-                          image: AssetImage('assets/album1.jpg'),
-                        ),
-                        SizedBox(
-                          width: 14,
-                        ),
-                        AlbumCard(
-                          label: 'Best Songs - Global',
-                          image: AssetImage('assets/album2.jpg'),
-                        ),
-                        SizedBox(
-                          width: 14,
-                        ),
-                        AlbumCard(
-                          label: 'Rap Mode',
-                          image: AssetImage('assets/album4.jpg'),
-                        ),
-                        SizedBox(
-                          width: 14,
-                        ),
-                        AlbumCard(
-                          label: 'Gaming Mode',
-                          image: AssetImage('assets/album5.jpg'),
-                        ),
-                      ],
+                    const SizedBox(
+                      width: 16,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 22,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'Good evening',
-                          style: Theme.of(context).textTheme.headlineSmall,
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            'Recommended radio',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
                         ),
-                        const SizedBox(
-                          height: 14,
-                        ),
-                        Row(
-                          children: const [
-                            RowAlbumCard(
-                              label: 'TOP 50 - Global',
-                              image: AssetImage('assets/top50.jpg'),
-                            ),
-                            SizedBox(
-                              width: 14,
-                            ),
-                            RowAlbumCard(
-                              label: 'Pop Remix',
-                              image: AssetImage('assets/album6.jpg'),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 14,
-                        ),
-                        Row(
-                          children: const [
-                            RowAlbumCard(
-                              label: 'TOP 50 - USA',
-                              image: AssetImage('assets/album13.jpg'),
-                            ),
-                            SizedBox(
-                              width: 14,
-                            ),
-                            RowAlbumCard(
-                              label: 'Workout',
-                              image: AssetImage('assets/album12.jpg'),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 14,
-                        ),
-                        Row(
-                          children: const [
-                            RowAlbumCard(
-                              label: 'Best of NF',
-                              image: AssetImage('assets/album9.jpg'),
-                            ),
-                            SizedBox(
-                              width: 14,
-                            ),
-                            RowAlbumCard(
-                              label: 'Beats to Study',
-                              image: AssetImage('assets/album10.jpg'),
-                            ),
-                          ],
-                        ),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          physics: const BouncingScrollPhysics(),
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Row(
+                            children: const [
+                              SongCard(
+                                image: AssetImage('assets/album13.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album3.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album2.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album10.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album9.jpg'),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              SongCard(
+                                image: AssetImage('assets/album6.jpg'),
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     ),
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          'Based on your recent listening',
-                          style: Theme.of(context).textTheme.headlineSmall,
-                        ),
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        physics: const BouncingScrollPhysics(),
-                      )
-                    ],
-                  )
-                ],
+                    const SizedBox(
+                      width: 16,
+                    ),
+                  ],
+                ),
               ),
             ),
           )
---
+++ lib/widgets/song_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SongCard extends StatelessWidget {
+  final AssetImage image;
+  const SongCard({
+    super.key,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 140,
+      child: Column(
+        children: [
+          Image(
+            image: image,
+            width: 140,
+            height: 140,
+          ),
+          Text(
+            'Travis Scott, Taylor Swift, Eminem, Drake, ...',
+            style: Theme.of(context).textTheme.bodySmall,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
