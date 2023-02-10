86e1fd814846532333a49a9a03269e33f1c64d45
Eryk Lorenc <reus2000@wp.pl>
2023-02-10 17:18:58 +0100

    Add profile view

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -10,9 +10,106 @@ class ProfileView extends StatefulWidget {
 class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile Works'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 30),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/user.jpg'),
+                  radius: 70,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: const [
+                        Text(
+                          '501',
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Music',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: const [
+                        Text(
+                          '5.1K',
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Column(
+                    children: const [
+                      Text(
+                        '2.3K',
+                        style: TextStyle(color: Colors.white, fontSize: 35),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Follow',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ))
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(200),
+                    ),
+                  ),
+                  child: const Text(
+                    'Edit Profile',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
