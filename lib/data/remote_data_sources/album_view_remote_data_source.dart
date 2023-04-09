4d7286cf3abfbf3e6860a921b16d114d40d00afd
Eryk Lorenc <reus2000@wp.pl>
2023-04-09 14:34:57 +0200

    Add dio handling errors

--- lib/data/remote_data_sources/album_view_remote_data_source.dart
+++ lib/data/remote_data_sources/album_view_remote_data_source.dart
@@ -1,10 +1,15 @@
 import 'package:dio/dio.dart';
 
 class AlbumViewRemoteDataSource {
-  Future<Map<String,dynamic>?> getDataAlbumView() async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'https://jsonplaceholder.typicode.com/posts/1');
-    return response.data;
-
+  Future<Map<String, dynamic>?> getDataAlbumView() async {
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'https://jsonplaceholder.typicode.com/posts/1');
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+        error.response?.data['error']['message'] ?? 'Unknown error',
+      );
+    }
   }
 }
