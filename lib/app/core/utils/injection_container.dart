96ac776c1893facd7a643509cc002b05904de2d9
eryklorenc <reus2000@wp.pl>
2023-11-16 19:14:45 +0100

    Add const

--- lib/app/core/utils/injection_container.dart
+++ lib/app/core/utils/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:spotify_ui_clone/app/core/config/const.dart';
 import 'package:spotify_ui_clone/app/core/utils/injection_container.config.dart';
 
 final getIt = GetIt.instance;
@@ -10,9 +11,6 @@ void configureDependencies() => getIt.init();
 
 @module
 abstract class RegisterModule {
-  @Named("BaseUrl")
-  String get baseUrl => 'https://jsonplaceholder.typicode.com';
-
   @lazySingleton
-  Dio dio(@Named('BaseUrl') String url) => Dio(BaseOptions(baseUrl: url));
+  Dio dio() => Dio(BaseOptions(baseUrl: Const.baseUrl));
 }
